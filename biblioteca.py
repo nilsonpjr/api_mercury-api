57127b0d7ad4ad8ea3edcccc8ba6329536f6f5f9
nilsonpjr <nilsonpjr@gmail.com>
2022-06-13 21:26:13 -0300

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,101 @@
+# from asyncore import dispatcher
+from importlib.metadata import entry_points
+from lib2to3.pgen2.token import EQUAL
+from pickle import FALSE
+from matplotlib.pyplot import text
+from pyparsing import null_debug_action
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+import bs4
+import webbrowser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+def conecta_login():
+    login = 31240
+    senha = "2105_kasa"
+    # item = "impeller"
+    options = Options()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument('window-size=200,400')
+
+    # Abre o Navegador e Entra no Site
+    navegador = webdriver.Chrome(chrome_options=options)
+    navegador.get("https://portal.mercurymarine.com.br/epdv/epdv001.asp")
+    # Insere o login e Senha
+    navegador.find_element(
+        By.XPATH, '/html/body/center/form/table/tbody/tr/td/table[2]/tbody/tr[2]/td[2]/input').send_keys(login)
+    navegador.find_element(
+        By.XPATH, '/html/body/center/form/table/tbody/tr/td/table[2]/tbody/tr[3]/td[2]/input').send_keys(senha)
+    navegador.find_element(
+        By.XPATH, '/html/body/center/form/table/tbody/tr/td/table[2]/tbody/tr[4]/td/input').send_keys(Keys.ENTER)
+    return navegador
+
+
+def dados_cliente(nro_motor):
+    navegador = conecta_login()
+    navegador.get(
+        "https://portal.mercurymarine.com.br/epdv/ewr010c.asp?s_nr_serie=" + nro_motor)
+    sleep(4)
+    navegador.switch_to.window(navegador.window_handles[0])
+    sleep(4)
+    nome_cli = navegador.find_element(
+        By.XPATH, '//*[@id="warranty_clients"]/table/tbody/tr/td/table[2]/tbody/tr[3]').text
+    nome_cli = nome_cli.upper()
+    return nome_cli
+
+
+def ConsultaGarantia(nro_motor):
+    navegador = conecta_login()
+    navegador.get(
+        "https://portal.mercurymarine.com.br/epdv/ewr010.asp?s_nr_serie=" + nro_motor)
+    sleep(5)
+    navegador.switch_to.window(navegador.window_handles[0])
+    sleep(5)
+    teste = navegador.find_element(
+        By.XPATH, '/html/body/table/tbody/tr/td/table[1]/tbody/tr/td[2]/strong/font').text
+    nro_motor = nro_motor.upper()
+    """Verifica se o termo digitado esta correto ou foi encontrado"""
+    if str(teste) != str(nro_motor):
+        print('Nenhum Motor encontrado para esse número de série!')
+        return '<h1> Nenhum Motor encontrado para esse numero de serie!</h1>'
+    else:
+        print('Sucesso! Motor encontrado')
+        nro_serie = navegador.find_element(
+            By.XPATH, '//*[@id="warr_cardnr_serie_1"]').text
+        modelo = navegador.find_element(
+            By.XPATH, '/html/body/table/tbody/tr/td/table[2]/tbody/tr[3]/td[2]').text
+        dt_venda = navegador.find_element(
+            By.XPATH, '/html/body/table/tbody/tr/td/table[2]/tbody/tr[3]/td[3]').text
+        status_garantia = navegador.find_element(
+            By.XPATH, '/html/body/table/tbody/tr/td/table[2]/tbody/tr[3]/td[5]').text
+        vld_garantia = navegador.find_element(
+            By.XPATH, '/html/body/table/tbody/tr/td/table[2]/tbody/tr[3]/td[6]').text
+        nome_cli = dados_cliente(nro_motor)
+        print(nro_serie)
+        modelo = modelo.replace("\n", '')
+        print(modelo)
+        print(dt_venda)
+        print(status_garantia)
+        print(vld_garantia)
+        nome_cli = str(nome_cli)
+        nome_cli = nome_cli.replace("NOME ", "")
+        print(str(nome_cli))
+        dados_pesq = dict()
+        for i in range(6):
+            dados_pesq = {
+                'nro_motor': nro_motor,
+                'nro_serie': nro_serie,
+                'modelo': modelo,
+                'dt_venda': dt_venda,
+                'status_garantia': status_garantia,
+                'vld_garantia': vld_garantia,
+                'nome_cli': nome_cli,
+            }
+        print(dados_pesq)
+        return dados_pesq
---
+++ funcoes.py
@@ -0,0 +1,117 @@
+from pickle import FALSE
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+import bs4
+import webbrowser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+def pesqpreco(item):
+    login = 31240
+    senha = "2105_kasa"
+    # item = "impeller"
+    options = Options()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument('window-size=200,400')
+
+    # Abre o Navegador e Entra no Site
+    navegador = webdriver.Chrome(chrome_options=options)
+    navegador.get("https://portal.mercurymarine.com.br/epdv/epdv001.asp")
+    # Insere o login e Senha
+    navegador.find_element(
+        By.XPATH, '/html/body/center/form/table/tbody/tr/td/table[2]/tbody/tr[2]/td[2]/input').send_keys(login)
+    navegador.find_element(
+        By.XPATH, '/html/body/center/form/table/tbody/tr/td/table[2]/tbody/tr[3]/td[2]/input').send_keys(senha)
+    navegador.find_element(
+        By.XPATH, '/html/body/center/form/table/tbody/tr/td/table[2]/tbody/tr[4]/td/input').send_keys(Keys.ENTER)
+
+    navegador.get(
+        "https://portal.mercurymarine.com.br/epdv/epdv002d2.asp?s_nr_pedido_web=11111111111111111&s_nr_tabpre=&s_fm_cod_com=null&s_desc_item=" + item)
+    sleep(4)
+
+    """
+        Verifica se o termo digitado esta correto ou foi encontrado
+        
+        """
+    teste = navegador.find_element(
+        By.XPATH, '/html/body/form[1]/table/tbody/tr/td/table[2]/tbody/tr[3]')
+
+    if teste.get_attribute("class") == 'NoRecords':
+        print('Nenhum registro encontrado - favor verificar !')
+        return FALSE
+    else:
+        print('Sucesso! item encontrado')
+
+    # Pegar valor da pesquisa codigo  nome  valor custo e valor venda
+    procura = navegador.page_source
+    site = BeautifulSoup(procura, 'html.parser')
+    linha = len(navegador.find_elements(
+        By.XPATH, '//*[@id="preco_item_web"]/table/tbody/tr/td/table[2]/tbody/tr'))
+    coluna = len(navegador.find_elements(
+        By.XPATH, '//*[@id="preco_item_web"]/table/tbody/tr/td/table[2]/tbody/tr[3]/td'))
+    # Print rows and columns
+    # print(linha)
+    coluna -= 1
+    linha -= 1
+    # itens = 0
+    # table = np.empty((linha, coluna), int)
+    # table1 = [1,2,3]
+    lista = []
+    descricao = []
+    qtdaEst = []
+    valor = 0
+    valorTabela = []
+    valorCusto = []
+    for r in range(3, int(linha + 1)):
+        for p in range(2, int(coluna + 1)):
+            element = navegador.find_element(
+                By.XPATH,
+                '//*[@id="preco_item_web"]/table/tbody/tr/td/table[2]/tbody/tr[' + str(r) + ']/td[' + str(p) + ']')
+            html_content = element.get_attribute('outerHTML')
+            soup = BeautifulSoup(html_content, 'html.parser')
+            table = soup.find('td')
+            lista.append(soup.find('td').text)
+
+    lista = [el.replace('\xa0',' ') for el in lista]
+
+    # print(lista)
+
+    # print(table.text)
+    valor = len(lista)
+    # print(f'Encontrado {valor / 5}')
+    # print(valor)
+
+    dicionario1 = dict()
+    lista1 = dict()
+    dicionario =list()
+    r = 0
+    for i in range(0, valor, 7):
+        dicionario1= {
+            'codigo': lista[i].replace(' ',''),
+            'qtd': lista[i+1],
+            'descricao': lista[i+2].replace('     ',''),
+            'qtdaEst': lista[i+3].replace(' ',''),
+            'valorVenda': lista[i+4].replace(' ',''),
+            'valorTabela': lista[i+5].replace(' ',''),
+            'valorCusto': lista[i+6].replace(' ',''),
+            # 'qtdEncontrada': valor/5
+        }
+        dicionario1.update(dicionario1)
+        dicionario.append(dicionario1)
+        print(dicionario)
+    print(dicionario)
+
+        # for e in dicionario:
+        #     for v in e.values():
+        #         print(v, end=' ')
+        #         print('')
+    # print(len(dicionario))
+    # qtdade_itens = len(dicionario)
+    # print(qtdade_itens)
+        # return str(dicionario[0].get("descricao"))
+    return dicionario
