e4d892e2912028aa6bfc308b6c06cb50adf6a6c4
nilsonpjr <nilsonpjr@gmail.com>
2022-09-28 21:46:22 -0300

    oi cors denovo

--- app.py
+++ app.py
@@ -13,25 +13,25 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
 # Rota Raiz
-CORS(app, resources={r"/preco/*": {"origins": "*"}})
-CORS(app, resources={r"/garantia/*": {"origins": "*"}})
+# CORS(app, resources={r"/preco/*": {"origins": "*"}})
+# CORS(app, resources={r"/garantia/*": {"origins": "*"}})
 CORS(app)
 @cross_origin
 @app.route("/")
-@cross_origin
+# @cross_origin
 def raiz():
     return "<h1>Olá : Mundo</h1>"
 
 @cross_origin
 @app.route("/preco/<item>")
-@cross_origin
+# @cross_origin
 def get_pesq_preco(item):
     dicionario = funcoes.pesqpreco(item)
     return json.dumps(dicionario)
 
 @cross_origin
 @app.route("/garantia/<nromotor>")
-@cross_origin
+# @cross_origin
 def get_garantia(nromotor):
     dicionario = biblioteca.ConsultaGarantia(nromotor)
     return json.dumps(dicionario)
